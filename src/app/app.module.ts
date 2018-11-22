9a313d26ddbfa109d4f34fa66409f121db62a594
Ker0h <ywillems96@gmail.com>
2018-11-22 11:12:16 +0100

    Added routing and select

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,26 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule } from '@angular/core'
+import { Routes, RouterModule } from '@angular/router'
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { SelectComponent } from './select/select.component';
+
+const routes: Routes = [
+  { path: 'select', component: SelectComponent },
+  { path: 'slider', component: SliderComponent },
+  { path: 'toggle', component: ToggleComponent }
+]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SelectComponent
   ],
   imports: [
     BrowserModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
